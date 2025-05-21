a0d71d8da0a902ff57f109a29b0d44621b4bef3e
Asrın Haliç <98893650+Asrinss@users.noreply.github.com>
2025-05-21 18:56:43 +0300

    Time: 8 ms (16.48%), Space: 55.6 MB (20.34%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const subsetXORSum = function(nums) {
+    let total = 0;
+    const n = nums.length;
+
+    for (let mask = 0; mask < (1 << n); mask++) {
+        let xor = 0;
+        for (let i = 0; i < n; i++) {
+            if (mask & (1 << i)) xor ^= nums[i];
+        }
+        total += xor;
+    }
+
+    return total;
+};
